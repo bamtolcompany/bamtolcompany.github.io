17a0e904a834e51f9daaea6e0b34b5488c3f2d81
bamtolcompany <mean119@naver.com>
2025-06-20 22:56:25 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
 const CACHE_NAME = 'allinone-search-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/robots.txt',
-  '/sitemap.xml'
-  // 필요하면 CSS, JS 파일도 추가
-];
+const faviconUrl = '/favicon.ico';
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.add(faviconUrl))
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.url.endsWith(faviconUrl)) {
+    // 파비콘 요청일 때만 캐시 확인
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  } else {
+    // 나머지 요청은 무조건 네트워크에서 받아오기
+    event.respondWith(fetch(event.request));
+  }
 });
