b45f51105ebe8d1ee70495583871dd7758228181
bamtolcompany <mean119@naver.com>
2025-09-06 17:23:35 +0900

    커밋

---
+++ README.md
@@ -0,0 +1,5 @@
+이 사이트의 url
+https://bamtolcompany.github.io/
+https://bamtolcompany.github.io/allinonenews/
+https://bamtolcompany.github.io/allinoneshopping/
+https://bamtolcompany.github.io/allinonemusic/
---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const weekNumber = Math.floor(Date.now() / (1000 * 60 * 60 * 24 * 7));
+const CACHE_NAME = `allinone-search-cache-week-${weekNumber}`;
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/manifest.json',
+  '/service-worker.js',
+  '/ads.txt',
+  '/robots.txt',
+  '/404.html'
+];
+
+// 설치: 캐시 저장
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 활성화: 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// 요청: 네트워크 우선, 실패 시 캐시
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        if (
+          networkResponse &&
+          networkResponse.status === 200 &&
+          event.request.method === 'GET' &&
+          event.request.url.startsWith(self.location.origin)
+        ) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
